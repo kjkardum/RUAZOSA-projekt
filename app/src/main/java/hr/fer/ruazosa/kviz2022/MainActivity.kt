edbadb1450841bf4d20625317f20d88ce40bae6b
ninjacornix <milanvrankic@gmail.com>
2022-02-12 17:35:25 +0100

    Revert "Login and Register fragments"
    This reverts commit e9a71554

--- app/src/main/java/hr/fer/ruazosa/kviz2022/MainActivity.kt
+++ app/src/main/java/hr/fer/ruazosa/kviz2022/MainActivity.kt
@@ -3,7 +3,6 @@ package hr.fer.ruazosa.kviz2022
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
