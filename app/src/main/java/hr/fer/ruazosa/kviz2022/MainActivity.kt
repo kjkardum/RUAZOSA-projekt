98c9eb92ea105fb60e4ba804927647a4c717ca26
Luka Hobor <luka.hobor@gmail.com>
2022-02-18 18:19:29 +0100

    nikakve znacajne promjene

--- app/src/main/java/hr/fer/ruazosa/kviz2022/MainActivity.kt
+++ app/src/main/java/hr/fer/ruazosa/kviz2022/MainActivity.kt
@@ -4,12 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import androidx.preference.PreferenceManager
 import dagger.hilt.android.AndroidEntryPoint
 import hr.fer.ruazosa.kviz2022.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -24,6 +27,16 @@ class MainActivity : AppCompatActivity() {
             if (!getBoolean(OnboardingActivity.COMPLETED_ONBOARDING_PREF_NAME, false)) {
                 val intent = Intent(this@MainActivity, OnboardingActivity::class.java)
                 startActivity(intent)
+            }
+        }
+        //binding.bottomNavigation.selectedItemId = R.id.homepageFragment
+        binding.bottomAppBar.setOnMenuItemClickListener{ item ->
+            when(item.itemId) {
+                R.id.homepageFragment -> {navController.navigate(R.id.action_leaderboardFragment2_to_homepageFragment2)
+                true}
+                R.id.leaderboardFragment -> {navController.navigate(R.id.action_homepageFragment2_to_leaderboardFragment2)
+                    true}
+                else -> false
             }
         }
     }
